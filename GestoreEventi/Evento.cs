129deb28e576d96bf290b832da0831694d9e03a6
Giuseppe Russo <russogiuseppe412@gmail.com>
2022-04-22 10:09:46 +0200

    Create Evento.cs

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace GestoreEventi
+{
+	public class Evento
+	{
+		public string titolo { get; set; }
+		public DateTime data { get; set; }
+		private int capienzaMassima;
+		private int numeroPostiPrenotati;
+
+		public Evento(string titolo, DateTime data, int capienzaMassima, int numeroPostiPrenotati)
+		{
+			this.titolo = titolo;
+			this.data = data;
+			this.capienzaMassima = capienzaMassima;
+			this.numeroPostiPrenotati = numeroPostiPrenotati;
+		}
+	}
+}
+
