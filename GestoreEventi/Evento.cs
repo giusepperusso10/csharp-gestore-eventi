3da4978188eed27a6ae3ce2b4e18a85c78eaaca1
Giuseppe Russo <russogiuseppe412@gmail.com>
2022-04-22 11:56:24 +0200

    PostiPrenotati

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -6,14 +6,87 @@ namespace GestoreEventi
 		public string titolo { get; set; }
 		public DateTime data { get; set; }
 		private int capienzaMassima;
+		private int postiDisponibili;
 		private int numeroPostiPrenotati;
+		private int postiRimanenti;
 
-		public Evento(string titolo, DateTime data, int capienzaMassima, int numeroPostiPrenotati)
+		public Evento(string titolo, DateTime data, int capienzaMassima)
 		{
 			this.titolo = titolo;
 			this.data = data;
 			this.capienzaMassima = capienzaMassima;
-			this.numeroPostiPrenotati = numeroPostiPrenotati;
+			this.postiDisponibili = capienzaMassima;
+			numeroPostiPrenotati = 0;
+		}
+
+		public string GetTitolo()
+        {
+			return titolo;
+        }
+
+		public string SetTitolo(string titolo)
+		{
+			return titolo;
+		}
+
+		public DateTime GetDateTime()
+        {
+			return data;
+        }
+
+		public DateTime SetDateTime(DateTime data)
+		{
+			ControlloDataEsatta();
+			return data;
+		}
+
+		public int GetCapienzaMassima()
+        {
+			return capienzaMassima;
+        }
+
+		public int GetNumeroPostiPrenotati()
+		{
+			return numeroPostiPrenotati;
+		}
+
+		public int GetNumeroPostiRimasti()
+		{
+			return postiRimanenti;
+		}
+
+		public int GetPostiDisponibili()
+		{
+			return postiDisponibili;
+		}
+
+		public void ControlloDataEsatta()
+		{
+
+			DateTime dataOraAttuale = DateTime.Now;
+
+			if (data <= dataOraAttuale)
+			{
+				throw new ArgumentOutOfRangeException("Non puoi inserire data nel passato");
+			}
+		}
+
+		public void PrenotaPosti(int postiDaPrenotare)
+        {
+			numeroPostiPrenotati = numeroPostiPrenotati + postiDaPrenotare;
+			postiDisponibili = postiDisponibili - postiDaPrenotare; 
+        }
+
+		public void DisdiciPosti(int postiDaDisdire)
+		{
+			numeroPostiPrenotati = numeroPostiPrenotati - postiDaDisdire;
+			postiDisponibili = postiDisponibili + postiDaDisdire;
+		}
+
+		public override string ToString()
+		{
+			Console.WriteLine(data + " - " + titolo);
+			return base.ToString();
 		}
 	}
 }
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,3 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using GestoreEventi;
 
+Console.Write("Inserisci il nome dell'evento: ");
+string? titolo = Console.ReadLine().ToLower();
+while (titolo == null)
+{
+    titolo = Console.ReadLine().ToLower();
+}
+
+DateTime data = DateTime.Now;
+bool dataCorretta = false;
+while (!dataCorretta)
+{
+        Console.Write("Aggiungi data [gg/mm/aaaa] e ora [00:00] : ");
+        try
+        {
+            data = DateTime.Parse(Console.ReadLine());
+            dataCorretta = true;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Il formato non è corretto");
+        }
+    
+
+}
+
+Console.Write("Inserisci il numero di posti totali: ");
+int capienzaMassima = int.Parse(Console.ReadLine().ToLower());
+while (capienzaMassima == null)
+{
+    capienzaMassima = int.Parse(Console.ReadLine().ToLower());
+}
+
+Console.Write("Quanti posti desideri prenotare?: ");
+int postiDaPrenotare = int.Parse(Console.ReadLine().ToLower());
+while (postiDaPrenotare == null)
+{
+    postiDaPrenotare = int.Parse(Console.ReadLine().ToLower());
+}
+
+
+
+Evento evento1 = new Evento(titolo, data, capienzaMassima);
+
+evento1.PrenotaPosti(postiDaPrenotare);
+
+Console.WriteLine("Numero posti prenotati: " + evento1.GetNumeroPostiPrenotati());
+Console.WriteLine("Numero numero posti disponibili: " + evento1.GetPostiDisponibili());
+
+bool uscita = false;
+while (!uscita)
+{
+    Console.Write("Vuoi disdire posti? (si/no): ");
+    string scelta = Console.ReadLine().ToLower();
+
+    switch (scelta)
+    {
+        case "si":
+            Console.Write("Quanti posti vuoi disdire?: ");
+            int postiDaDisdire = int.Parse(Console.ReadLine().ToLower());
+            while (postiDaDisdire == null)
+            {
+                postiDaDisdire = int.Parse(Console.ReadLine().ToLower());
+            }
+            evento1.DisdiciPosti(postiDaDisdire);
+            Console.WriteLine("Numero posti prenotati: " + evento1.GetNumeroPostiPrenotati());
+            Console.WriteLine("Numero numero posti disponibili: " + evento1.GetPostiDisponibili());
+            break;
+        case "no":
+            Console.WriteLine("Grazie");
+            Console.WriteLine("");
+            uscita = true;
+            break;
+        default:
+            Console.WriteLine("Mi dispiace opzione non contemplata");
+            break;
+    }
+}
